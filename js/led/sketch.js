395b100b435e618e7923e1bd9a82075cae67483d
Oscar <etherealflux94@gmail.com>
2016-06-24 13:39:19 -0500

    Mejora LED: Selecciona otra imagen al azar cuando llega a su fin de recorrido

--- js/led/sketch.js
+++ js/led/sketch.js
@@ -3,7 +3,7 @@ var galaxia;
 var x = -200;
 
 //Array de imágenes de galaxias
-var galaxias = ["../js/led/orion.jpg", "../js/led/cosmos.jpg", "../js/led/swan.jpg", "../js/led/the-eagle.jpg"];
+var galaxias = ["../js/led/orion.jpg", "../js/led/cosmos.jpg", "../js/led/swan.jpg", "../js/led/eagle.jpg"];
 var galaxiaElegida;
 
 
@@ -22,14 +22,16 @@ function draw() {
   background(0);
   x += 0.05;
   push();
+  if (x >= -100) {
+    galaxiaElegida = (int(random(4)));
+    galaxia = loadImage(galaxias[galaxiaElegida]);
+    x = -200;
+  }
   image(galaxia, x, 0);
   pop();
+
+  //Máscara
   image(led, 0, 0);
-
-  if (x >= 400) {
-    x = 0;
-  }
-
-
+  console.log(x);
 
 }
