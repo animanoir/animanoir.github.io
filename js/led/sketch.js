659d7f911dc5e541b2692fd2d30070528c3c7a9a
Oscar <etherealflux94@gmail.com>
2016-06-24 12:56:51 -0500

    Mejora de LED

--- js/led/sketch.js
+++ js/led/sketch.js
@@ -1,26 +1,32 @@
 var led;
 var galaxia;
-var x;
+var x = -200;
+
+//Array de imágenes de galaxias
+var galaxias = ["../js/led/orion.jpg", "../js/led/cosmos.jpg", "../js/led/swan.jpg", "../js/led/the-eagle.jpg"];
+var galaxiaElegida;
 
 
 function setup() {
   //Usa un div con id p5 como canvas.
   var cnv = createCanvas(301, 455);
   cnv.parent("p5");
-  galaxia = loadImage("../js/led/andromeda.jpg");
+
+  //Selecciona un index de galaxias al azar
+  galaxiaElegida = (int(random(4)));
+  galaxia = loadImage(galaxias[galaxiaElegida]);
   led = loadImage("../js/led/led-transparente.png");
-  x = 0;
 }
 
 function draw() {
   background(0);
-  x += 0.1;
+  x += 0.05;
   push();
   image(galaxia, x, 0);
   pop();
   image(led, 0, 0);
 
-  if (x >= 100) {
+  if (x >= 400) {
     x = 0;
   }
 
